54991b844b415ccb420b29e731cedda9bf5ee837
cuttestkittensrule <mangoiscute95@gmail.com>
2026-01-22 20:19:18 -0800

    Add a test to make sure that we can create a robot container and run a periodic cycle without crashing

---
+++ src/test/java/com/team2813/RobotContainerTest.java
@@ -0,0 +1,19 @@
+package com.team2813;
+
+import edu.wpi.first.wpilibj2.command.Commands;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(WPILibExtension.class)
+public class RobotContainerTest {
+  @Test
+  public void canCreateRobotAndRunPeriodic(CommandTester tester) {
+    Assumptions.assumeTrue(
+        Constants.simMode == Constants.Mode.SIM, "The sim mode must be sim to run tests!");
+    // create a robot container
+    RobotContainer robotContainer = new RobotContainer();
+    // Run one periodic cycle
+    tester.runUntilComplete(Commands.none());
+  }
+}
