75abf3697127709063c2546353686c556e4da185
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-01-24 15:00:26 -0800

    Changed CANIDs to match prototype

--- src/main/java/com/team2813/Constants.java
+++ src/main/java/com/team2813/Constants.java
@@ -30,10 +30,13 @@ public final class Constants {
   }
 
   // CAN IDs
-  public static final int MAIN_ROLLER_MOTOR_CAN_ID = 18;
-  public static final int FOLLOWER_MOTOR_MOTOR_CAN_ID = 19;
 
-  // When robot front is facing away, this is the right vectoring motor.
-  public static final int RIGHT_FEEDER_MOTOR_ID = 20;
-  public static final int LEFT_FEEDER_MOTOR_ID = 21;
+  // AKA: Magazine motors
+  public static final int MAIN_ROLLER_MOTOR_CAN_ID = 25; // Right roller motor.
+  public static final int FOLLOWER_ROLLER_MOTOR_CAN_ID = 22; // Left roller motor.
+
+  // When robot front is facing away, this is the right feeder motor.
+  // AKA: Vectoring motors
+  public static final int LEFT_FEEDER_MOTOR_ID = 23;
+  public static final int RIGHT_FEEDER_MOTOR_ID = 24;
 }
