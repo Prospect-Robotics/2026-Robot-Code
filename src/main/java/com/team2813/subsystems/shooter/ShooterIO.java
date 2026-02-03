e87dcc15056c571eda1291889b25bad8d4b51a60
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-02-03 12:29:58 -0800

    Added more values to shooter IO

--- src/main/java/com/team2813/subsystems/shooter/ShooterIO.java
+++ src/main/java/com/team2813/subsystems/shooter/ShooterIO.java
@@ -1,4 +1,19 @@
 package com.team2813.subsystems.shooter;
 
+import org.littletonrobotics.junction.AutoLog;
+
 public interface ShooterIO {
+
+    @AutoLog
+    class ShooterIOInputs {}
+
+    /**
+     * Updates Advantage kit autologged input data, as well as any other necessary states (like in
+     * sim)
+     *
+     * @param inputs The "struct" (data class) to handle hardware inputs.
+     */
+    default void updateState(ShooterIOInputs inputs) {}
+
+    default void setMotorVoltage() {}
 }
