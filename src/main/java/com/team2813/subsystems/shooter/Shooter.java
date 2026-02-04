a6f6fce9175ad86e9ce8c6bc5e7f7e49ea20a0a1
Stefan Dikov <118777573+spderman3333@users.noreply.github.com>
2026-02-03 21:51:46 -0800

    Added inputs for the IO interface

--- src/main/java/com/team2813/subsystems/shooter/Shooter.java
+++ src/main/java/com/team2813/subsystems/shooter/Shooter.java
@@ -3,5 +3,9 @@ package com.team2813.subsystems.shooter;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Shooter extends SubsystemBase {
-    public Shooter() {}
+    private final ShooterIO io;
+
+    public Shooter(ShooterIO io) {
+        this.io = io;
+    }
 }
--- src/main/java/com/team2813/subsystems/shooter/ShooterIO.java
+++ src/main/java/com/team2813/subsystems/shooter/ShooterIO.java
@@ -1,11 +1,28 @@
 package com.team2813.subsystems.shooter;
 
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Current;
+import edu.wpi.first.units.measure.Voltage;
 import org.littletonrobotics.junction.AutoLog;
+
+import static edu.wpi.first.units.Units.*;
 
 public interface ShooterIO {
 
     @AutoLog
-    class ShooterIOInputs {}
+    class ShooterIOInputs {
+        public Voltage mainShooterMotorVoltage = Volts.of(0);
+        public AngularVelocity mainShooterMotorRPS = RotationsPerSecond.of(0);
+        public Current mainShooterMotorCurrent = Amps.of(0);
+
+        public Voltage followerShooterMotorVoltage = Volts.of(0);
+        public AngularVelocity followerShooterMotorRPS = RotationsPerSecond.of(0);
+        public Current followerShooterMotorCurrent = Amps.of(0);
+
+        public Voltage kickerShooterMotorVoltage = Volts.of(0);
+        public AngularVelocity kickerShooterMotorRPS = RotationsPerSecond.of(0);
+        public Current kickerShooterMotorCurrent = Amps.of(0);
+    }
 
     /**
      * Updates Advantage kit autologged input data, as well as any other necessary states (like in
