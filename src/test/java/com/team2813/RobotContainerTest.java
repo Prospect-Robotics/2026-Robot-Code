f163af861e2f90671a5cbe423dd423a291a22437
cuttestkittensrule <mangoiscute95@gmail.com>
2026-02-08 11:06:42 -0800

    Explicitly use rev's maven url instead of adding a vendordep on REVLib

--- src/test/java/com/team2813/RobotContainerTest.java
+++ src/test/java/com/team2813/RobotContainerTest.java
@@ -1,8 +1,8 @@
 package com.team2813;
 
-import com.team2813.subsystems.drive.AllDrivetrains;
 import com.team2813.lib2813.testing.junit.jupiter.CommandTester;
 import com.team2813.lib2813.testing.junit.jupiter.WPILibExtension;
+import com.team2813.subsystems.drive.AllDrivetrains;
 import edu.wpi.first.wpilibj2.command.Commands;
 import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
