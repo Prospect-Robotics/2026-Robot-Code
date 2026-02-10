f32157506770c82f1fdc9808086ad34f257902fd
Piyou <tj67542007@gmail.com>
2026-02-09 19:44:57 -0800

    serial number code

--- src/main/java/com/team2813/Robot.java
+++ src/main/java/com/team2813/Robot.java
@@ -70,7 +70,7 @@ public class Robot extends LoggedRobot {
     // Instantiate our RobotContainer. This will perform all our button bindings,
     // and put our autonomous chooser on the dashboard.
 
-    robotContainer = new RobotContainer(AllDrivetrains.drWomp());
+    robotContainer = new RobotContainer(AllDrivetrains.forRoboRIO());
   }
 
   /** This function is called periodically during all modes. */
--- src/main/java/com/team2813/subsystems/drive/AllDrivetrains.java
+++ src/main/java/com/team2813/subsystems/drive/AllDrivetrains.java
@@ -1,8 +1,14 @@
 package com.team2813.subsystems.drive;
 
 import com.team2813.generated.drwomp.TunerConstants;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.RobotController;
+import java.util.Map;
 
 public class AllDrivetrains {
+  static Map<String, AllTunerConstants> robotConstants = Map.of("", drWomp());
 
   public static AllTunerConstants drWomp() {
     AllTunerConstants robotConstants =
@@ -12,5 +18,17 @@ public class AllDrivetrains {
             TunerConstants.BackLeft,
             TunerConstants.BackRight);
     return robotConstants;
+  }
+
+  public static AllTunerConstants forRoboRIO() {
+    String sNumber = RobotController.getSerialNumber();
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("Metadata");
+    table.getStringTopic("RoboRIO Serial Number").publish().set(sNumber);
+    var tunerConstants = robotConstants.get(sNumber);
+    if (tunerConstants == null) {
+      // tunerConstants = drWomp();
+      DriverStation.reportError("no tuner constants for serial number", null);
+    }
+    return tunerConstants;
   }
 }
