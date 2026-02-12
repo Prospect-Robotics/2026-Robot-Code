2de019123b00c1abe362e265378084bf4fbcd618
codingman106 <jazl.ihsan@gmail.com>
2026-02-11 18:49:06 -0800

    fixes more

--- src/main/java/com/team2813/subsystems/vision/Vision.java
+++ src/main/java/com/team2813/subsystems/vision/Vision.java
@@ -21,7 +21,6 @@ import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import org.littletonrobotics.junction.Logger;
