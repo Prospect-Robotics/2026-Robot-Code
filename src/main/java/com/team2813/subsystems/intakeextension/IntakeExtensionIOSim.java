d135e07dea88f0868f68b4e8c39a15a6bca7a162
Veselin Dikov <veselin.dikov@gmail.com>
2026-02-16 12:17:40 -0800

    Figured out the motor/extension inversion problem; simplified the code quite a bit

--- src/main/java/com/team2813/subsystems/intakeextension/IntakeExtensionIOSim.java
+++ src/main/java/com/team2813/subsystems/intakeextension/IntakeExtensionIOSim.java
@@ -1,24 +1,33 @@
 package com.team2813.subsystems.intakeextension;
 
-import static edu.wpi.first.units.Units.*;
+import static edu.wpi.first.units.Units.Kilograms;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Rotation;
 
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.sim.TalonFXSimState;
 import com.team2813.Constants;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.wpilibj.simulation.ElevatorSim;
-import org.littletonrobotics.junction.Logger;
 
 public class IntakeExtensionIOSim implements IntakeExtensionIO {
-
   private final TalonFX extenderMotor;
   private final TalonFXSimState extenderMotorSimState;
 
   private final ElevatorSim extenderSim;
 
   private Angle extensionSetpoint;
+
+  // Motor to extension direction depends on how the motor is geared with respect to the extension
+  // mechanism.
+  private static final double MOTOR_DIRECTION =
+      IntakeExtensionConstants.EXTENDER_MOTOR_CONFIG.MotorOutput.Inverted
+              == InvertedValue.CounterClockwise_Positive
+          ? 1.0
+          : -1.0;
 
   public IntakeExtensionIOSim() {
     extenderMotor = new TalonFX(Constants.EXTENDER_MOTOR_CAN_ID);
@@ -42,9 +51,29 @@ public class IntakeExtensionIOSim implements IntakeExtensionIO {
 
   @Override
   public void updateState(IntakeExtensionIOInputs inputs) {
-    updateSimulation();
+    // Continue supplying the simulated motor with 12V voltage.
     extenderMotorSimState.setSupplyVoltage(12);
 
+    // Read back the actual motor control voltage as calculated by the extenderMotorSimState,
+    // which internally takes the setpoint and position error + PID Values into account.
+    extenderSim.setInput(MOTOR_DIRECTION * extenderMotorSimState.getMotorVoltage());
+
+    // With the new motor voltage, step forward the simulation by 20ms. This will update the
+    // position and velocity of the simulated mechanism.
+    extenderSim.update(0.02);
+
+    // No the simulated physical movement of the mechanism is fed back into the motor simulation so
+    // that its simulated PID controler can give us new simulated motor voltage next time around.
+    extenderMotorSimState.setRotorVelocity(
+        MOTOR_DIRECTION
+            * extenderSim.getVelocityMetersPerSecond()
+            * IntakeExtensionConstants.DISTANCE_METERS_TO_MOTOR_ROTATIONS);
+    extenderMotorSimState.setRawRotorPosition(
+        MOTOR_DIRECTION
+            * extenderSim.getPositionMeters()
+            * IntakeExtensionConstants.DISTANCE_METERS_TO_MOTOR_ROTATIONS);
+
+    // Finally update all simulated inputs.
     inputs.extenderMotorVoltage = extenderMotor.getMotorVoltage().getValue();
     inputs.extenderMotorRPS = extenderMotor.getRotorVelocity().getValue();
     inputs.extenderMotorCurrent = extenderMotor.getStatorCurrent().getValue();
@@ -56,26 +85,5 @@ public class IntakeExtensionIOSim implements IntakeExtensionIO {
   public void setExtensionSetpoint(Angle setpoint) {
     extensionSetpoint = setpoint;
     extenderMotor.setControl(new PositionVoltage(setpoint));
-  }
-
-  public void updateSimulation() {
-    extenderSim.setInput(extenderMotorSimState.getMotorVoltage());
-
-    extenderSim.update(0.02);
-
-    extenderMotorSimState.setRotorVelocity(
-        (extenderSim.getVelocityMetersPerSecond()
-            * IntakeExtensionConstants.DISTANCE_METERS_TO_MOTOR_ROTATIONS));
-    extenderMotorSimState.setRawRotorPosition(
-        extenderSim.getPositionMeters()
-            * IntakeExtensionConstants.DISTANCE_METERS_TO_MOTOR_ROTATIONS);
-
-    Logger.recordOutput(
-        "IntakeExtensionSim/extensionSimVelocity",
-        MetersPerSecond.of(extenderSim.getVelocityMetersPerSecond()));
-    Logger.recordOutput(
-        "IntakeExtensionSim/extensionSimLength", Meters.of(extenderSim.getPositionMeters()));
-    Logger.recordOutput("IntakeExtensionSim/hasHitLowerLimit", extenderSim.hasHitLowerLimit());
-    Logger.recordOutput("IntakeExtensionSim/hasHitUpperLimit", extenderSim.hasHitUpperLimit());
   }
 }
