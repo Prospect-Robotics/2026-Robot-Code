152cc44b2eeedecdc999f6bf64cf97c89d1f49ae
atlanticbomber <atlanticbomber@gmail.com>
2026-02-16 18:09:08 -0800

    changed controls for intake

--- src/main/java/com/team2813/RobotContainer.java
+++ src/main/java/com/team2813/RobotContainer.java
@@ -182,16 +182,11 @@ public class RobotContainer {
                 }));
 
     // Feeder and Vectoring Bindings
-    driveController.leftBumper().onTrue(hopper.intakeCommand()).onFalse(hopper.stopCommand());
-    driveController.rightBumper().onTrue(hopper.outtakeCommand()).onFalse(hopper.stopCommand());
-
-    // Shooter Bindings
-    driveController.leftTrigger().onTrue(shooter.intakeCommand()).onFalse(shooter.stopCommand());
-    driveController.rightTrigger().onTrue(shooter.outakeCommand()).onFalse(shooter.stopCommand());
+    
 
     // Intake Roller Bindings
-    operatorController.leftBumper().whileTrue(intakeRoller.intakeCommand());
-    operatorController.rightBumper().whileTrue(intakeRoller.outtakeCommand());
+    driveController.rightBumper().whileTrue(intakeRoller.intakeCommand());
+    operatorController.leftTrigger().whileTrue(intakeRoller.outtakeCommand());
 
     // Intake Extension Bindings
     intakeExtension.setDefaultCommand(
