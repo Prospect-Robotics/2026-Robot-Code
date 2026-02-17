2f5086cf7ee48b652073680c5de1e027f5605fe4
vdikov <120233643+vdikov@users.noreply.github.com>
2026-02-16 20:37:49 -0800

    Add mapplesim (#42)
    The code was generated by studying (with simulating and running through debugger) https://github.com/Pearadox/2025RobotCode and borrowing some classes and routines from it.
    
    There were hiccups along the way with maplesim not working well with some CTRE lib configs, but fortunately there was fix eventually found on https://www.chiefdelphi.com/t/maplesim-strange-behavior-need-help/502245/6. The code affected by this fix has a reference to that. 
    
    The main idea of the simulation is that the `RobotContainer` holds a maplesim drive simulation object:
    
    ```
    SwerveDriveSimulation driveSimulation;
    ```
    
    Then, the drive simulation is added to a maplesim Arena simulation:
    
    ```
    SimulatedArena.getInstance().addDriveTrainSimulation(driveSimulation);
    ```
    
    Finally, the `RobotContainer` has two members that reset and update the Simulated arena: `resetSimulation()` and `displaySimFieldToAdvantageScope()`, resp. These two methods are then called from `Robot`'s `disableInit()` and `simulationPeriodic()`, resp. ModuleIOSim.java and GyroIOSim.java were updated to work with maplesim's simulation framework. That was borrowed from Pearadox's code and my deserve more studying. 
    
    During simulation, the simulated pose that respects collisions with the field is published under:
    
    `/RealOutputs/FieldSimulation/Pose`.
    
    Similarly, the simulated gamepieces are published under:
    
    `/RealOutputs/FieldSimulation/Fuel`.
    
    (Select "Gamepiece" when showing the "Fuel" pose and it the fuel pieces in 3D Field of Advantage Scope will come to life!)

--- src/main/java/com/team2813/Robot.java
+++ src/main/java/com/team2813/Robot.java
@@ -93,7 +93,9 @@ public class Robot extends LoggedRobot {
 
   /** This function is called once when the robot is disabled. */
   @Override
-  public void disabledInit() {}
+  public void disabledInit() {
+    robotContainer.resetSimulation();
+  }
 
   /** This function is called periodically when disabled. */
   @Override
@@ -148,5 +150,7 @@ public class Robot extends LoggedRobot {
 
   /** This function is called periodically whilst in simulation. */
   @Override
-  public void simulationPeriodic() {}
+  public void simulationPeriodic() {
+    robotContainer.displaySimFieldToAdvantageScope();
+  }
 }
