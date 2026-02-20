08adc8fdea46b9ee1f0a865e26abb23450c88b06
Stefan Dikov <118777573+spderman3333@users.noreply.github.com>
2026-02-19 17:33:35 -0800

    Spotless.

--- src/main/java/com/team2813/Robot.java
+++ src/main/java/com/team2813/Robot.java
@@ -7,9 +7,9 @@
 
 package com.team2813;
 
-import com.team2813.util.SimulationVisualizer;
 import com.team2813.subsystems.drive.AllDrivetrains;
 import com.team2813.subsystems.drive.AllTunerConstants;
+import com.team2813.util.SimulationVisualizer;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import org.littletonrobotics.junction.LogFileUtil;
