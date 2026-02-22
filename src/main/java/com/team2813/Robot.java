166e083bca46131867a0eb2c1379f4627263ccfa
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-02-22 14:30:32 -0800

    Spotless apply

--- src/main/java/com/team2813/Robot.java
+++ src/main/java/com/team2813/Robot.java
@@ -10,7 +10,6 @@ package com.team2813;
 import com.team2813.subsystems.drive.AllDrivetrains;
 import com.team2813.subsystems.drive.AllTunerConstants;
 import com.team2813.util.SimulationVisualizer;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
