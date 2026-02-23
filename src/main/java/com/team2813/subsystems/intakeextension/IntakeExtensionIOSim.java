8099c1e767a264bce9a6ed1bfb62ccceb95b3dd7
Veselin Dikov <veselin.dikov@gmail.com>
2026-02-22 16:04:51 -0800

    Set simulated motor orientation via extenderMotorSimState.Orientation and remove the MOTOR_DIRECTION constant.

--- src/main/java/com/team2813/subsystems/intakeextension/IntakeExtensionIOSim.java
+++ src/main/java/com/team2813/subsystems/intakeextension/IntakeExtensionIOSim.java
@@ -4,6 +4,7 @@ import static edu.wpi.first.units.Units.Kilograms;
 import static edu.wpi.first.units.Units.Meters;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.sim.ChassisReference;
 import com.ctre.phoenix6.sim.TalonFXSimState;
 import edu.wpi.first.math.system.plant.DCMotor;
@@ -13,14 +14,6 @@ public class IntakeExtensionIOSim implements IntakeExtensionIO {
   TalonFX extenderMotor;
   private TalonFXSimState extenderMotorSimState;
   private ElevatorSim extenderSim;
-
-  // Motor to extension direction depends on how the motor is geared with respect to the extension
-  // mechanism.
-  private static final double MOTOR_DIRECTION = 1.0;
-  // IntakeExtensionConstants.EXTENDER_MOTOR_CONFIG.MotorOutput.Inverted
-  //         == InvertedValue.CounterClockwise_Positive
-  //     ? 1.0
-  //     : -1.0;
 
   public IntakeExtensionIOSim() {
     extenderSim =
@@ -39,19 +32,23 @@ public class IntakeExtensionIOSim implements IntakeExtensionIO {
   public void setMotor(TalonFX motor) {
     extenderMotor = motor;
     extenderMotorSimState = motor.getSimState();
-    extenderMotorSimState.Orientation = ChassisReference.Clockwise_Positive;
+    extenderMotorSimState.Orientation =
+        IntakeExtensionConstants.EXTENDER_MOTOR_CONFIG.MotorOutput.Inverted
+                == InvertedValue.CounterClockwise_Positive
+            ? ChassisReference.CounterClockwise_Positive
+            : ChassisReference.Clockwise_Positive;
     extenderMotorSimState.setMotorType(TalonFXSimState.MotorType.KrakenX44);
   }
 
   @Override
   public void updateState(IntakeExtensionIOInputs inputs) {
-    extenderMotorSimState = extenderMotor.getSimState();
+    // extenderMotorSimState = extenderMotor.getSimState();
     // Continue supplying the simulated motor with 12V voltage.
     extenderMotorSimState.setSupplyVoltage(12);
 
     // Read back the actual motor control voltage as calculated by the extenderMotorSimState,
     // which internally takes the setpoint and position error + PID Values into account.
-    extenderSim.setInput(MOTOR_DIRECTION * extenderMotorSimState.getMotorVoltage());
+    extenderSim.setInput(extenderMotorSimState.getMotorVoltage());
 
     // With the new motor voltage, step forward the simulation by 20ms. This will update the
     // position and velocity of the simulated mechanism.
@@ -61,12 +58,9 @@ public class IntakeExtensionIOSim implements IntakeExtensionIO {
     // that its simulated PID controler can give us new simulated motor voltage next time around.
     double extenderSimPositionsInM = extenderSim.getPositionMeters();
     extenderMotorSimState.setRotorVelocity(
-        MOTOR_DIRECTION
-            * extenderSim.getVelocityMetersPerSecond()
+        extenderSim.getVelocityMetersPerSecond()
             * IntakeExtensionConstants.DISTANCE_METERS_TO_MOTOR_ROTATIONS);
     extenderMotorSimState.setRawRotorPosition(
-        MOTOR_DIRECTION
-            * extenderSimPositionsInM
-            * IntakeExtensionConstants.DISTANCE_METERS_TO_MOTOR_ROTATIONS);
+        extenderSimPositionsInM * IntakeExtensionConstants.DISTANCE_METERS_TO_MOTOR_ROTATIONS);
   }
 }
