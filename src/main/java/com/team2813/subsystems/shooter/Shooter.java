5a35d0b29a4a2f1cfe1cb79c45e3d9ecbe76331e
Jazl <jazl.ihsan@gmail.com>
2026-02-23 23:29:07 +0000

    spotless

--- src/main/java/com/team2813/subsystems/shooter/Shooter.java
+++ src/main/java/com/team2813/subsystems/shooter/Shooter.java
@@ -1,7 +1,6 @@
 package com.team2813.subsystems.shooter;
 
 import static edu.wpi.first.units.Units.*;
-import static edu.wpi.first.units.Units.Volts;
 
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj2.command.*;
