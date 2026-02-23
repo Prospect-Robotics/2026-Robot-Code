710d1ac91459c4312cad210f2061b6eedd32fbe1
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-02-23 14:57:15 -0800

    Deleted unused code.

--- src/main/java/com/team2813/subsystems/shooter/Shooter.java
+++ src/main/java/com/team2813/subsystems/shooter/Shooter.java
@@ -27,29 +27,6 @@ public class Shooter extends SubsystemBase {
     io.setMotorVoltages(Volts.of(0), Volts.of(0));
   }
 
-  // Waits before starting the kicker to allow the shooter flywheel to get up to speed.
-  //  public Command intakeCommand() {
-  //    return new SequentialCommandGroup(
-  //            new ParallelCommandGroup(
-  //                new InstantCommand(
-  //                    () ->
-  // io.setShooterMotorVoltage(ShooterConstants.getShooterIntakeVoltage())),
-  //                new InstantCommand(
-  //                    () ->
-  // io.setKickerMotorVoltage(ShooterConstants.getKickerOuttakeVoltage()))),
-  //            new WaitCommand(Seconds.of(2)),
-  //            new StartEndCommand(
-  //                () -> io.setKickerMotorVoltage(ShooterConstants.getKickerIntakeVoltage()),
-  //                this::stop,
-  //                this))
-  //        .finallyDo(this::stop);
-  //    /*
-  //    Note: I still use StartEndCommand because it only calls the first Runnable once,
-  //     rather than repeatedly like RunCommand.
-  //     Thus I believe it will save on hardware calls, but it could just be over engineering.
-  //     */
-  //  }\
-
   public Command spoolShooterInstantIntakeCommand() {
     return new InstantCommand(
         () -> io.setShooterMotorVoltage(ShooterConstants.getShooterIntakeVoltage()), this);
