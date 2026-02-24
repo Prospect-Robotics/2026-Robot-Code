0fed1af55ae92eb84df922c1b62defe67dc8d5cd
codingduck106 <jazl.ihsan@gmail.com>
2026-02-23 16:32:58 -0800

    the beginnings

--- src/test/java/com/team2813/subsystems/intakeextension/IntakeExtensionTest.java
+++ src/test/java/com/team2813/subsystems/intakeextension/IntakeExtensionTest.java
@@ -1,81 +1,23 @@
 package com.team2813.subsystems.intakeextension;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import com.team2813.lib2813.testing.junit.jupiter.InitWPILib;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
-import org.junitpioneer.jupiter.ClearEnvironmentVariable;
+import edu.wpi.first.hal.DriverStationJNI;
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.RuntimeType;
+import edu.wpi.first.wpilibj.simulation.DriverStationSim;
+import edu.wpi.first.wpilibj.simulation.SimHooks;
+import org.junit.jupiter.api.BeforeAll;
 
-@Disabled
-@InitWPILib
-@ClearEnvironmentVariable(key = "FRC_ADVANTAGEKIT_LOG_REPLAY_ENABLE")
 public class IntakeExtensionTest {
-  @Test
-  public void testIntakeExtension() {
-    // create an intake extension subsystem
 
-    IntakeExtension intakeExtension = new IntakeExtension(new IntakeExtensionIOSim());
+  @BeforeAll
+  public static void init() {
+    HAL.initialize(500, 1);
 
-    // extend the intake
-    intakeExtension.extend();
-
-    for (int i = 0; i < 50; i++) {
-      intakeExtension.periodic();
-    }
-
-    assertEquals(
-        intakeExtension.getSetpoint().magnitude(),
-        IntakeExtensionConstants.toMotorSetpoint(IntakeExtensionConstants.ExtenderPositions.OUT)
-            .magnitude(),
-        0.01);
+    DriverStationSim.setEnabled(true);
+    DriverStationSim.notifyNewData();
+    SimHooks.setHALRuntimeType(RuntimeType.kSimulation.value);
   }
 
-  @Disabled
-  @Test
-  public void testIntakeRetraction() {
-    // create an intake extension subsystem
 
-    IntakeExtension intakeExtension = new IntakeExtension(new IntakeExtensionIOSim());
-
-    // retract the intake
-    intakeExtension.retract();
-
-    for (int i = 0; i < 50; i++) {
-      intakeExtension.periodic();
-    }
-
-    assertEquals(
-        intakeExtension.getSetpoint().magnitude(),
-        IntakeExtensionConstants.toMotorSetpoint(IntakeExtensionConstants.ExtenderPositions.IN)
-            .magnitude(),
-        0.01);
-  }
-
-  @Disabled
-  @Test
-  public void testIntakeExtensionAtPosition() {
-    // create an intake extension subsystem
-
-    IntakeExtension intakeExtension = new IntakeExtension(new IntakeExtensionIOSim());
-
-    // The intake should start at the retracted position, so the extender should be at position
-    assertTrue(intakeExtension.isExtenderAtPosition());
-
-    // extend the intake
-    intakeExtension.extend();
-    // With new setpoint set by extend(), the extender should no longer be at position.
-    assertFalse(intakeExtension.isExtenderAtPosition());
-
-    // run periodic at the equivalent of 50 cycles (1 second) to let the intake reach the setpoint
-    for (int i = 0; i < 50; i++) {
-      intakeExtension.periodic();
-    }
-
-    // TODO: Jazl, please debug this, it seems the PID, and kSVA constants for the Intake have not
-    // been merged properly.
-    assertTrue(intakeExtension.isExtenderAtPosition());
-  }
 }
