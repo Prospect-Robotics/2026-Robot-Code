108a51fd101aaf7422d36b6b2c50ea390f72ad7c
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-02-25 18:17:14 -0800

    Minor Sim tweaks.

--- src/main/java/com/team2813/subsystems/climb/ClimbIOSim.java
+++ src/main/java/com/team2813/subsystems/climb/ClimbIOSim.java
@@ -5,6 +5,7 @@ import static edu.wpi.first.units.Units.*;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.sim.TalonFXSimState;
+import com.team2813.Constants;
 import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.units.measure.Angle;
@@ -44,6 +45,9 @@ public class ClimbIOSim implements ClimbIO {
   // Used for actually moving the motor to a given position with PID applied to a voltage input.
   private final PositionVoltage positionControl = new PositionVoltage(Rotations.of(0));
 
+  // -1 for inverted, 1 for forward motor. Used because the sim is inverted comapared to real life.
+  private final double MOTOR_INVERTED = -1.0;
+
   public ClimbIOSim() {
     innerMotor = new TalonFX(0);
     innerMotor.getConfigurator().apply(ClimbConstants.INNER_MOTOR_TO_CLIMB_CONFIG);
@@ -72,13 +76,12 @@ public class ClimbIOSim implements ClimbIO {
 
   private void updateSim() {
     innerMotorSim.setSupplyVoltage(Volts.of(12));
-    double motorInverted = -1.0; // -1 for inverted, 1 for forward motor.
 
     // Apply the voltage to the sim elevator that we apply to the sim motor.
     // Negating the sim motor value since it is set to use negative value when pushing
     // the cartrage UP.
-    innerClimbSim.setInputVoltage(motorInverted * innerMotorSim.getMotorVoltage());
-    innerClimbSim.update(0.02); // Same update cycle as an actual robot, 20 ms.
+    innerClimbSim.setInputVoltage(MOTOR_INVERTED * innerMotorSim.getMotorVoltage());
+    innerClimbSim.update(Constants.SIM_TIME_PERIOD); // Same update cycle as an actual robot, 20 ms.
 
     // Logs to "Real Outputs" NT
     Logger.recordOutput("Simulated Climb/motorSim/Voltage", innerMotorSim.getMotorVoltage());
@@ -97,11 +100,11 @@ public class ClimbIOSim implements ClimbIO {
         "Simulated Climb/climbSim/hitsLowerLimit", outerClimbSim.hasHitLowerLimit());
 
     innerMotorSim.setRawRotorPosition(
-        motorInverted * getLeftMotorRotations(innerClimbSim.getPositionMeters()));
+        MOTOR_INVERTED * getLeftMotorRotations(innerClimbSim.getPositionMeters()));
 
     // angular velocity = linear velocity / radius, taken also from 5414
     innerMotorSim.setRotorVelocity(
-        motorInverted
+        MOTOR_INVERTED
             * ((innerClimbSim.getVelocityMetersPerSecond()
                     / ClimbConstants.INNER_CLIMB_SPOOL_RADIUS.in(Meters))
                 // radians/sec to rotations/sec
@@ -113,7 +116,7 @@ public class ClimbIOSim implements ClimbIO {
     // Apply the voltage to the sim elevator that we apply to the sim motor.
     // Negating the sim motor value since it is set to use negative value when pushing
     // the cartrage UP.
-    outerClimbSim.setInputVoltage(motorInverted * outerMotorSim.getMotorVoltage());
+    outerClimbSim.setInputVoltage(MOTOR_INVERTED * outerMotorSim.getMotorVoltage());
     outerClimbSim.update(0.02); // Same update cycle as an actual robot, 20 ms.
 
     // Logs to "Real Outputs" NT
@@ -133,11 +136,11 @@ public class ClimbIOSim implements ClimbIO {
         "Simulated Climb/climbSim/hitsLowerLimit", outerClimbSim.hasHitLowerLimit());
 
     innerMotorSim.setRawRotorPosition(
-        motorInverted * getRightMotorRotations(outerClimbSim.getPositionMeters()));
+        MOTOR_INVERTED * getRightMotorRotations(outerClimbSim.getPositionMeters()));
 
     // angular velocity = linear velocity / radius, taken also from 5414
     innerMotorSim.setRotorVelocity(
-        motorInverted
+        MOTOR_INVERTED
             * ((innerClimbSim.getVelocityMetersPerSecond()
                     / ClimbConstants.OUTER_CLIMB_SPOOL_RADIUS.in(Meters))
                 // radians/sec to rotations/sec
