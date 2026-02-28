468b68b96c45a26730581f8f9435a8015f1589b1
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-02-27 16:14:35 -0800

    Added methods to increase/decrease the current voltages.

--- src/main/java/com/team2813/subsystems/shooter/Shooter.java
+++ src/main/java/com/team2813/subsystems/shooter/Shooter.java
@@ -3,6 +3,7 @@ package com.team2813.subsystems.shooter;
 import static edu.wpi.first.units.Units.*;
 
 import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.*;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import org.littletonrobotics.junction.Logger;
@@ -15,6 +16,7 @@ public class Shooter extends SubsystemBase {
   private final static String TRENCH_VOLTAGE_INCREASE_BUTTON_KEY = "TRENCH_SHOOTER_VOLTAGE_INCREASE";
   private final static String TRENCH_VOLTAGE_DECREASE_BUTTON_KEY = "TRENCH_SHOOTER_VOLTAGE_DECREASE";
 
+  // Shows the current set voltage
   private final static String TRENCH_CURRENT_VOLTAGE_REPORTER_KEY = "TRENCH_SHOOTER_CURRENT_VOLTAGE";
 
   // double in order to save on object initialization
@@ -26,11 +28,50 @@ public class Shooter extends SubsystemBase {
 
   private final static String HUB_CURRENT_VOLTAGE_REPORTER_KEY = "HUB_SHOOTER_CURRENT_VOLTAGE";
 
-  private double currentHubShooterVoltage = 5.4;
+  private double currentHubShooterVoltage = 5.5;
 
   public Shooter(ShooterIO io) {
     this.io = io;
     this.replayedInputs = new ShooterIOInputsAutoLogged();
+
+
+
+  }
+
+  /**
+   * Increase the voltage for a trench shot by 0.5.
+   */
+  private void increaseTrenchShooterVoltage() {
+    if (currentHubShooterVoltage <= 13)  { // Max voltage is 13.
+      currentTrenchShooterVoltage += 0.5;
+    }
+  }
+
+  /**
+   * Decrease the voltage for a trench shot by 0.5.
+   */
+  private void decreaseTrenchShooterVoltage() {
+    if (currentTrenchShooterVoltage >= 0)  { // Min voltage is 0
+      currentTrenchShooterVoltage -= 0.5;
+    }
+  }
+
+  /**
+   * Increase the voltage for a hub shot by 0.5.
+   */
+  private void increaseHubShooterVoltage() {
+    if (currentHubShooterVoltage <= 13) {
+      currentHubShooterVoltage += 0.5;
+    }
+  }
+
+  /**
+   * Decrease the voltage for a hub shot by 0.5.
+   */
+  private void decreaseHubShooterVoltage() {
+    if  (currentHubShooterVoltage >= 0)  {
+      currentHubShooterVoltage -= 0.5;
+    }
   }
 
   @Override
@@ -41,6 +82,9 @@ public class Shooter extends SubsystemBase {
 
     Logger.recordOutput("Shooter/currentTrenchShooterVoltage", currentTrenchShooterVoltage);
     Logger.recordOutput("Shooter/currentHubShooterVoltage", currentHubShooterVoltage);
+
+
+    SmartDashboard.updateValues();
   }
 
   public void stop() {
