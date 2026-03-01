d6ec911e16a72359eeea6d30efca62084ad4ba99
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-02-28 17:27:40 -0800

    Update at 'Sat Feb 28 17:27:40 PST 2026'

--- src/main/java/com/team2813/subsystems/shooter/ShooterConstants.java
+++ src/main/java/com/team2813/subsystems/shooter/ShooterConstants.java
@@ -36,7 +36,7 @@ public class ShooterConstants {
    * this value of Rot/s of the setpoint, {@link Shooter#isMotorVelocityWithinTolerance()} will
    * return true.
    */
-  public static final AngularVelocity SHOOTER_SPOOL_SPEED_TOLERANCE = RotationsPerSecond.of(6);
+  public static final AngularVelocity SHOOTER_SPOOL_SPEED_TOLERANCE = RotationsPerSecond.of(25);
 
   static {
     // Shooter motors.
