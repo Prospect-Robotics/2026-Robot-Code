0e7a850cfe77f864c8ec920bd5ef8ac79dcc5a7c
Stefan <118777573+spderman3333@users.noreply.github.com>
2026-03-05 11:22:33 -0800

    Lowered shooter spool speed tolerance. (#93)

--- src/main/java/com/team2813/subsystems/shooter/ShooterConstants.java
+++ src/main/java/com/team2813/subsystems/shooter/ShooterConstants.java
@@ -36,7 +36,7 @@ public class ShooterConstants {
    * this value of Rot/s of the setpoint, {@link Shooter#isMotorVelocityWithinTolerance()} will
    * return true.
    */
-  public static final AngularVelocity SHOOTER_SPOOL_SPEED_TOLERANCE = RotationsPerSecond.of(25);
+  public static final AngularVelocity SHOOTER_SPOOL_SPEED_TOLERANCE = RotationsPerSecond.of(2);
 
   static {
     // Shooter motors.
