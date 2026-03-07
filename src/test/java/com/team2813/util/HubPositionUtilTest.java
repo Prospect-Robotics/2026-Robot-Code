3abdca1146fbdddb0453776e13d87f78e74b4187
Stefan Dikov <118777573+spderman3333@users.noreply.github.com>
2026-03-07 10:24:42 -0800

    Made a tolerance for unit tests.

--- src/test/java/com/team2813/util/HubPositionUtilTest.java
+++ src/test/java/com/team2813/util/HubPositionUtilTest.java
@@ -20,7 +20,7 @@ public class HubPositionUtilTest {
         HubPositionUtil.getBotToHubDistance(testPosition, Optional.of(DriverStation.Alliance.Blue));
 
     // The distance from bot to hub is sqrt(4^2+4.48^2), calculated by java it is this number.
-    Assertions.assertEquals(6.080822312812635, distanceFromHub.in(Meters));
+    Assertions.assertEquals(6.080822312812635, distanceFromHub.in(Meters), 1e-5);
   }
 
   @Test
@@ -31,7 +31,7 @@ public class HubPositionUtilTest {
         HubPositionUtil.getBotToHubDistance(testPosition, Optional.of(DriverStation.Alliance.Red));
 
     // The distance from bot to hub is sqrt(4^2+11.812^2), calculated by java it is this number.
-    Assertions.assertEquals(12.47089988733772, distanceFromHub.in(Meters));
+    Assertions.assertEquals(12.47089988733772, distanceFromHub.in(Meters), 1e-5);
   }
 
   @Test
@@ -41,7 +41,7 @@ public class HubPositionUtilTest {
     Rotation2d angleFromHub =
         HubPositionUtil.getBotToHubAngle(testPosition, Optional.of(DriverStation.Alliance.Blue));
 
-    Assertions.assertEquals(0.7179017820664226, angleFromHub.getRadians());
+    Assertions.assertEquals(0.7179017820664226, angleFromHub.getRadians(), 1e-5);
   }
 
   @Test
@@ -51,6 +51,6 @@ public class HubPositionUtilTest {
     Rotation2d angleFromHub =
         HubPositionUtil.getBotToHubAngle(testPosition, Optional.of(DriverStation.Alliance.Red));
 
-    Assertions.assertEquals(0.3265177360538555, angleFromHub.getRadians());
+    Assertions.assertEquals(0.3265177360538555, angleFromHub.getRadians(), 1e-5);
   }
 }
