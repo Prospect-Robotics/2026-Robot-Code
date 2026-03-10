7bad3bcd6e830f05184c55a25eaf098d0c77affa
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-03-09 20:38:48 -0700

    Added inner climb to AllClimbs

--- src/main/java/com/team2813/subsystems/climb/AllClimbs.java
+++ src/main/java/com/team2813/subsystems/climb/AllClimbs.java
@@ -1,11 +1,35 @@
 package com.team2813.subsystems.climb;
 
 public class AllClimbs {
-private static AllClimbConstants outerClimb(){
-    AllClimbConstants climbConstants = 
-        new AllClimbConstants("Outer", ClimbConstants.OUTER_MOTOR_TO_CLIMB_GEARING, ClimbConstants.OUTER_MOTOR_TO_CLIMB_CONFIG, ClimbConstants.OUTER_CLIMB_HEIGHT_CHANGE_PER_MOTOR_ROTATION, 
-        ClimbConstants.OUTER_CLIMB_SPOOL_RADIUS, ClimbConstants.OUTER_CLIMB_MIN_HEIGHT, ClimbConstants.OUTER_CLIMB_MIN_HEIGHT); 
+    public static AllClimbConstants outerClimb() {
+        AllClimbConstants climbConstants =
+                new AllClimbConstants(
+                        "Outer",
+                        ClimbConstants.OUTER_MOTOR_TO_CLIMB_GEARING,
+                        ClimbConstants.OUTER_MOTOR_TO_CLIMB_CONFIG,
+                        ClimbConstants.OUTER_CLIMB_HEIGHT_CHANGE_PER_MOTOR_ROTATION,
+                        ClimbConstants.OUTER_CLIMB_SPOOL_RADIUS,
+                        ClimbConstants.OUTER_CLIMB_MIN_HEIGHT,
+                        ClimbConstants.OUTER_CLIMB_MAX_HEIGHT);
 
-    return climbConstants; 
+        return climbConstants;
     }
+
+    public static AllClimbConstants innerClimb() {
+        AllClimbConstants climbConstants =
+                new AllClimbConstants(
+                        "Inner",
+                        ClimbConstants.INNER_MOTOR_TO_CLIMB_GEARING,
+                        ClimbConstants.INNER_MOTOR_TO_CLIMB_CONFIG,
+                        ClimbConstants.INNER_CLIMB_HEIGHT_CHANGE_PER_MOTOR_ROTATION,
+                        ClimbConstants.INNER_CLIMB_SPOOL_RADIUS,
+                        ClimbConstants.INNER_CLIMB_MIN_HEIGHT,
+                        ClimbConstants.INNER_CLIMB_MAX_HEIGHT
+
+                );
+
+        return climbConstants;
+    }
+
+
 }
