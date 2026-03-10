a331a4fbd5da832812e79eba9d5637c4c924640d
Piyou <tj67542007@gmail.com>
2026-03-09 17:38:12 -0700

    debugging

--- src/main/java/com/team2813/subsystems/climb/Climb.java
+++ src/main/java/com/team2813/subsystems/climb/Climb.java
@@ -137,6 +137,7 @@ public class Climb extends SubsystemBase {
   public Command l2Sequence() {
     return new SequentialCommandGroup(
         l1Sequence(),
+        new WaitCommand(1),
         setOuterClimbPositionCommand(OuterClimbHeight.DOWN),
         new WaitCommand(1),
         setInnerClimbPositionCommand(InnerClimbHeight.UP),
@@ -149,6 +150,7 @@ public class Climb extends SubsystemBase {
   public Command l3Sequence() {
     return new SequentialCommandGroup(
         l2Sequence(),
+        new WaitCommand(1),
         setOuterClimbPositionCommand(OuterClimbHeight.DOWN),
         new WaitCommand(1),
         setInnerClimbPositionCommand(InnerClimbHeight.UP),
