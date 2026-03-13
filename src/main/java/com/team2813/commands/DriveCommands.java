6abdee0385171f31331cde49f962cfd4edff7d91
Stefan Dikov <118777573+spderman3333@users.noreply.github.com>
2026-03-12 18:52:16 -0700

    Removed unintentional code edit

--- src/main/java/com/team2813/commands/DriveCommands.java
+++ src/main/java/com/team2813/commands/DriveCommands.java
@@ -3,7 +3,7 @@
 //
 // Use of this source code is governed by a BSD
 // license that can be found in the LICENSE file
-// at the root directory of this project.  private Voltage manualMotorVoltage = Volt.of(3);
+// at the root directory of this project.
 
 package com.team2813.commands;
 
