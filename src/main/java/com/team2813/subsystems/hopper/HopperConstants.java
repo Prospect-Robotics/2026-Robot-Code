7d182c9bda2e9f50ed5986ae7d20a9347aacfaa6
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-03-15 09:30:45 -0700

    Refactored HopperConstants, and increased voltages

--- src/main/java/com/team2813/subsystems/hopper/HopperConstants.java
+++ src/main/java/com/team2813/subsystems/hopper/HopperConstants.java
@@ -12,29 +12,29 @@ public class HopperConstants {
 
   public static final double ROLLER_SIM_MOI = 0.00057684; // in kilograms*meters squared.
 
-  public static final String HOPPER_INTAKE_VOLTAGE_NT = "Hopper/ROLLER_INTAKE_VOLTAGE";
-  public static final String HOPPER_OUTTAKE_VOLTAGE_NT = "Hopper/ROLLER_OUTTAKE_VOLTAGE";
+  public static final String FEEDER_INTAKE_VOLTAGE_NT = "Hopper/FEEDER_INTAKE_VOLTAGE";
+  public static final String FEEDER_OUTTAKE_VOLTAGE_NT = "Hopper/FEEDER_OUTTAKE_VOLTAGE";
 
-  public static final String INDEXER_INTAKE_VOLTAGE_NT = "Hopper/FEEDER_INTAKE_VOLTAGE";
-  public static final String INDEXER_OUTTAKE_VOLTAGE_NT = "Hopper/FEEDER_OUTTAKE_VOLTAGE";
+  public static final String INDEXER_INTAKE_VOLTAGE_NT = "Hopper/INDEXER_INTAKE_VOLTAGE";
+  public static final String INDEXER_OUTTAKE_VOLTAGE_NT = "Hopper/INDEXER_OUTTAKE_VOLTAGE";
 
   static {
-    // Roller motors.
-    Preferences.initDouble(HOPPER_INTAKE_VOLTAGE_NT, 6.5);
-    Preferences.initDouble(HOPPER_OUTTAKE_VOLTAGE_NT, -8);
+    // Feeder motors.
+    Preferences.initDouble(FEEDER_INTAKE_VOLTAGE_NT, 8);
+    Preferences.initDouble(FEEDER_OUTTAKE_VOLTAGE_NT, -8);
 
-    // Feeder/Vectoring motors.
-    Preferences.initDouble(INDEXER_INTAKE_VOLTAGE_NT, 5);
+    // Indexer/Vectoring motors.
+    Preferences.initDouble(INDEXER_INTAKE_VOLTAGE_NT, 8);
     Preferences.initDouble(INDEXER_OUTTAKE_VOLTAGE_NT, -8);
   }
 
   // Roller Motor Configs
   public static Voltage getRollerIntakeVoltage() {
-    return Volts.of(Preferences.getDouble(HOPPER_INTAKE_VOLTAGE_NT, 6.5)); // 5 is the backup.
+    return Volts.of(Preferences.getDouble(FEEDER_INTAKE_VOLTAGE_NT, 6.5)); // 5 is the backup.
   }
 
   public static Voltage getRollerOuttakeVoltage() {
-    return Volts.of(Preferences.getDouble(HOPPER_OUTTAKE_VOLTAGE_NT, -8));
+    return Volts.of(Preferences.getDouble(FEEDER_OUTTAKE_VOLTAGE_NT, -8));
   }
 
   // TOP
