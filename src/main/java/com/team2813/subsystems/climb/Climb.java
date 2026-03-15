e1169c6d0ae465f528976b3850425184655d143c
Veselin Dikov <veselin.dikov@gmail.com>
2026-03-14 22:07:22 -0700

    Remove unused import

--- src/main/java/com/team2813/subsystems/climb/Climb.java
+++ src/main/java/com/team2813/subsystems/climb/Climb.java
@@ -10,7 +10,6 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.function.Supplier;
-import org.littletonrobotics.junction.AutoLogOutput;
 import org.littletonrobotics.junction.Logger;
 
 /**
