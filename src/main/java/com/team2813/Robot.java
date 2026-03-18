1c69abf071f4c43136ad6fc2efa8ddd3e81a8ddb
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-03-18 16:28:50 -0700

    Switched logging to a FAT32 formatted USB at /U/logs

--- src/main/java/com/team2813/Robot.java
+++ src/main/java/com/team2813/Robot.java
@@ -55,7 +55,7 @@ public class Robot extends LoggedRobot {
     switch (mode) {
       case REAL:
         // Running on a real robot, log to a USB stick ("/U/logs")
-        Logger.addDataReceiver(new WPILOGWriter("/home/lvuser/logs"));
+        Logger.addDataReceiver(new WPILOGWriter());
         Logger.addDataReceiver(new NT4Publisher());
         break;
 
