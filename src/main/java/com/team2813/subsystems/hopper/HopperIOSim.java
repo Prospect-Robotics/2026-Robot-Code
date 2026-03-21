382117b7159a256f90c53b4ad304fc352f42efb2
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-03-20 18:17:45 -0700

    Fixed hopper motor logging

--- src/main/java/com/team2813/subsystems/hopper/HopperIOSim.java
+++ src/main/java/com/team2813/subsystems/hopper/HopperIOSim.java
@@ -63,14 +63,17 @@ public class HopperIOSim implements HopperIO {
     inputs.mainFeederMotorVoltage = mainFeederMotor.getMotorVoltage().getValue();
     inputs.mainFeederMotorRPS = mainFeederMotor.getRotorVelocity().getValue();
     inputs.mainFeederMotorStatorCurrent = mainFeederMotor.getStatorCurrent().getValue();
+    inputs.mainFeederMotorSupplyCurrent = mainFeederMotor.getSupplyCurrent().getValue();
 
     inputs.followerFeederMotorVoltage = followerFeederMotor.getMotorVoltage().getValue();
     inputs.followerFeederMotorRPS = followerFeederMotor.getRotorVelocity().getValue();
     inputs.followerFeederMotorStatorCurrent = followerFeederMotor.getStatorCurrent().getValue();
+    inputs.followerFeederMotorSupplyCurrent = followerFeederMotor.getSupplyCurrent().getValue();
 
     inputs.indexerMotorVoltage = indexerMotor.getMotorVoltage().getValue();
     inputs.indexerMotorRPS = indexerMotor.getRotorVelocity().getValue();
     inputs.indexerMotorStatorCurrent = indexerMotor.getStatorCurrent().getValue();
+    inputs.indexerMotorSupplyCurrent = indexerMotor.getSupplyCurrent().getValue();
   }
 
   public void updateSimulation() {
