05202576bc41e52140e3984fea43476ba8bf75d8
Stefan Dikov <118777573+spderman3333@users.noreply.github.com>
2026-04-05 21:12:30 -0700

    Added second motor to IntakeRollerIOSim.

--- src/main/java/com/team2813/subsystems/intakeroller/IntakeRollerIOSim.java
+++ src/main/java/com/team2813/subsystems/intakeroller/IntakeRollerIOSim.java
@@ -11,49 +11,67 @@ import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.simulation.FlywheelSim;
 
 public class IntakeRollerIOSim implements IntakeRollerIO {
-  private TalonFX intakeMotor;
-  private TalonFXSimState intakeSimState;
+  private TalonFX leftIntakeMotor;
+  private TalonFXSimState leftIntakeSimState;
+
+  private TalonFX rightIntakeMotor;
+  private TalonFXSimState rightIntakeSimState;
 
   private FlywheelSim intakeFlywheelSim;
 
   public IntakeRollerIOSim() {
-    intakeMotor = new TalonFX(Constants.LEFT_INTAKE_MOTOR_CAN_ID);
-    intakeMotor.getConfigurator().apply(IntakeRollerConstants.LEFT_INTAKE_MOTOR_CONFIG);
+    leftIntakeMotor = new TalonFX(Constants.LEFT_INTAKE_MOTOR_CAN_ID);
+    leftIntakeMotor.getConfigurator().apply(IntakeRollerConstants.LEFT_INTAKE_MOTOR_CONFIG);
 
-    intakeSimState = intakeMotor.getSimState();
+    rightIntakeMotor = new TalonFX(Constants.RIGHT_INTAKE_MOTOR_CAN_ID);
+    rightIntakeMotor.getConfigurator().apply(IntakeRollerConstants.RIGHT_INTAKE_MOTOR_CONFIG);
+
+    leftIntakeSimState = leftIntakeMotor.getSimState();
+    rightIntakeSimState = rightIntakeMotor.getSimState();
 
     intakeFlywheelSim =
         new FlywheelSim(
             LinearSystemId.createFlywheelSystem(
-                DCMotor.getKrakenX60(1),
+                DCMotor.getKrakenX60(2),
                 IntakeRollerConstants.INTAKE_SIM_MOI,
                 IntakeRollerConstants.INTAKE_MOTOR_TO_INTAKE_GEARING),
-            DCMotor.getKrakenX60(1));
+            DCMotor.getKrakenX60(2));
   }
 
   @Override
   public void updateState(IntakeRollerIOInputs inputs) {
     updateSimulation();
 
-    intakeSimState.setSupplyVoltage(Volts.of(12));
+    leftIntakeSimState.setSupplyVoltage(Volts.of(12));
+    rightIntakeSimState.setSupplyVoltage(Volts.of(12));
 
-    inputs.leftIntakeMotorVoltage = intakeMotor.getMotorVoltage().getValue();
-    inputs.leftIntakeMotorRPS = intakeMotor.getVelocity().getValue();
-    inputs.leftIntakeMotorStatorCurrent = intakeMotor.getStatorCurrent().getValue();
-    inputs.leftIntakeMotorSupplyCurrent = intakeMotor.getSupplyCurrent().getValue();
+    inputs.leftIntakeMotorVoltage = leftIntakeMotor.getMotorVoltage().getValue();
+    inputs.leftIntakeMotorRPS = leftIntakeMotor.getVelocity().getValue();
+    inputs.leftIntakeMotorStatorCurrent = leftIntakeMotor.getStatorCurrent().getValue();
+    inputs.leftIntakeMotorSupplyCurrent = leftIntakeMotor.getSupplyCurrent().getValue();
+
+    inputs.rightIntakeMotorVoltage = rightIntakeMotor.getMotorVoltage().getValue();
+    inputs.rightIntakeMotorRPS = rightIntakeMotor.getVelocity().getValue();
+    inputs.rightIntakeMotorStatorCurrent = rightIntakeMotor.getStatorCurrent().getValue();
+    inputs.rightIntakeMotorSupplyCurrent = rightIntakeMotor.getSupplyCurrent().getValue();
   }
 
   public void updateSimulation() {
     // TODO: Once we fetch from main, change this to the SIM_TIME in Constants.
     intakeFlywheelSim.update(0.02);
 
-    intakeSimState.setRotorAcceleration(intakeFlywheelSim.getAngularAcceleration());
-    intakeSimState.setRotorVelocity(intakeFlywheelSim.getAngularVelocity());
+    leftIntakeSimState.setRotorAcceleration(intakeFlywheelSim.getAngularAcceleration());
+    leftIntakeSimState.setRotorVelocity(intakeFlywheelSim.getAngularVelocity());
+
+    rightIntakeSimState.setRotorAcceleration(intakeFlywheelSim.getAngularAcceleration());
+    rightIntakeSimState.setRotorVelocity(intakeFlywheelSim.getAngularVelocity());
   }
 
   @Override
   public void setIntakeMotorVoltage(Voltage intakeMotorVoltage) {
-    intakeMotor.setVoltage(intakeMotorVoltage.in(Volts));
+    leftIntakeMotor.setVoltage(intakeMotorVoltage.in(Volts));
+    rightIntakeMotor.setVoltage(intakeMotorVoltage.in(Volts));
+
     intakeFlywheelSim.setInputVoltage(intakeMotorVoltage.in(Volts));
   }
 }
