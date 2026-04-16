813c0309c68d5434acd8b92356557b985ba9eda1
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-04-15 17:32:48 -0700

    Hood readablity changes.

--- src/main/java/com/team2813/subsystems/hood/HoodConstants.java
+++ src/main/java/com/team2813/subsystems/hood/HoodConstants.java
@@ -5,7 +5,6 @@ import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.signals.NeutralModeValue;
-
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
@@ -23,11 +22,12 @@ class HoodConstants {
       new TalonFXConfiguration()
           // TODO: Run sysid to get PID values
           .withSlot0(new Slot0Configs().withKP(1).withKI(0).withKD(0).withKS(0).withKA(0))
-          .withFeedback(new FeedbackConfigs().withRotorToSensorRatio(HOOD_GEAR_RATIO)).withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake));
+          .withFeedback(new FeedbackConfigs().withRotorToSensorRatio(HOOD_GEAR_RATIO))
+          .withMotorOutput(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake));
 
-  static final String HUB_ANGLE_PREFERENCE = "Hub/hubAngle";
+  static final String HUB_ANGLE_PREFERENCE = "Hood/hubAngle";
   static final double DEFAULT_HUB_ANGLE = 45;
-  static final String TRENCH_ANGLE_PREFERENCE = "Hub/trenchAngle";
+  static final String TRENCH_ANGLE_PREFERENCE = "Hood/trenchAngle";
   static final double DEFAULT_TRENCH_ANGLE = 30;
 
   private HoodConstants() {
