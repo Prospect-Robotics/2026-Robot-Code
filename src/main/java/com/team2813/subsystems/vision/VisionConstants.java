824ec80fb14aeacc4aae2a92ab1c7ab27dc0d750
Stefan Dikov <118777573+spderman3333@users.noreply.github.com>
2026-04-18 11:52:47 -0700

    Added red and green positions where green is on the right, and red is on the left (may need to tune).

--- src/main/java/com/team2813/subsystems/vision/VisionConstants.java
+++ src/main/java/com/team2813/subsystems/vision/VisionConstants.java
@@ -39,13 +39,13 @@ public class VisionConstants {
   // (Not used by Limelight, configure in web UI instead)
   public static final Transform3d RED_BACK_LEFT_CAM_FROM_ROBOT =
       new Transform3d(
-          new Translation3d(Centimeters.of(-26), Centimeters.of(26), Centimeters.of(23)),
-          new Rotation3d(Degrees.of(0), Degrees.of(-15), Degrees.of(-160)));
+          new Translation3d(Inches.of(-7.802), Inches.of(13.188), Inches.of(7.125)),
+          new Rotation3d(Degrees.of(0), Degrees.of(-15), Degrees.of(55)));
 
   public static final Transform3d GREEN_BACK_RIGHT_CAM_FROM_ROBOT =
       new Transform3d(
-          new Translation3d(Centimeters.of(-26), Centimeters.of(-26), Centimeters.of(23)),
-          new Rotation3d(Degrees.of(0), Degrees.of(-15), Degrees.of(-90)));
+          new Translation3d(Inches.of(-7.802), Inches.of(-13.188), Inches.of(7.125)),
+          new Rotation3d(Degrees.of(0), Degrees.of(-15), Degrees.of(-55)));
 
   public static final Transform3d BLUE_FRONT_CAM_FROM_ROBOT =
       new Transform3d(
