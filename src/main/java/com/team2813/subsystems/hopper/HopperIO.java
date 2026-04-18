f08a67736793f8ee1de80af79cb71a9fc300e6e0
Jazl Jalin <jazl.ihsan@gmail.com>
2026-04-18 15:27:45 -0700

    removed input fields of follower model

--- src/main/java/com/team2813/subsystems/hopper/HopperIO.java
+++ src/main/java/com/team2813/subsystems/hopper/HopperIO.java
@@ -18,11 +18,6 @@ public interface HopperIO {
     public Current mainFeederMotorStatorCurrent = Amps.of(0);
     public Current mainFeederMotorSupplyCurrent = Amps.of(0);
 
-    public Voltage followerFeederMotorVoltage = Volts.of(0);
-    public AngularVelocity followerFeederMotorRPS = RotationsPerSecond.of(0);
-    public Current followerFeederMotorStatorCurrent = Amps.of(0);
-    public Current followerFeederMotorSupplyCurrent = Amps.of(0);
-
     // Feeder/Vector
     public Voltage indexerMotorVoltage = Volts.of(0);
     public AngularVelocity indexerMotorRPS = RotationsPerSecond.of(0);
