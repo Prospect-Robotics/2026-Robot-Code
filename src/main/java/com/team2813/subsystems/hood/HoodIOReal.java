3ecd061184f07cc0127cfd16d7e3465a07a917b5
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-04-18 14:10:06 -0700

    Minor comment tweak.

--- src/main/java/com/team2813/subsystems/hood/HoodIOReal.java
+++ src/main/java/com/team2813/subsystems/hood/HoodIOReal.java
@@ -18,7 +18,7 @@ public class HoodIOReal implements HoodIO {
     motor = new TalonFX(Constants.HOOD_MOTOR_ID);
     motor.getConfigurator().apply(HoodConstants.PIVOT_MOTOR_CONFIG);
 
-    // NOTE: May need to set this to 0.
+    // NOTE: This is set to 0, as the minimum angle is 0.
     motor.setPosition(HoodConstants.MINIMUM_SHOOTER_ANGLE.times(HoodConstants.HOOD_GEAR_RATIO));
   }
 
