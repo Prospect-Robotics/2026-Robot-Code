f18790ef9232ff4dda617934eef1412701d01ad0
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-04-18 13:40:30 -0700

    Addressed first half of reviews

--- src/main/java/com/team2813/subsystems/hood/Hood.java
+++ src/main/java/com/team2813/subsystems/hood/Hood.java
@@ -1,6 +1,5 @@
 package com.team2813.subsystems.hood;
 
-import static com.team2813.subsystems.hood.HoodConstants.HOOD_MOVEMENT_TIMEOUT;
 import static edu.wpi.first.units.Units.*;
 
 import edu.wpi.first.units.measure.Angle;
@@ -10,14 +9,13 @@ import org.littletonrobotics.junction.Logger;
 
 public class Hood extends SubsystemBase {
   private final HoodIO io;
-  private HoodIOInputsAutoLogged replayedInputs;
+  private final HoodIOInputsAutoLogged replayedInputs = new HoodIOInputsAutoLogged();
 
   // Note: This variable may be a little delayed to the actual position.
   private boolean hoodAtPosition = true;
 
   public Hood(HoodIO io) {
     this.io = io;
-    this.replayedInputs = new HoodIOInputsAutoLogged();
   }
 
   @Override
@@ -25,29 +23,29 @@ public class Hood extends SubsystemBase {
     Logger.processInputs("Hood", replayedInputs);
     io.updateState(replayedInputs);
 
-    double hoodError = replayedInputs.motorAngle.minus(replayedInputs.motorSetpoint).abs(Rotations);
+    double hoodMotorAngleAbsError = replayedInputs.motorAngle.minus(replayedInputs.motorSetpoint).abs(Rotations);
 
-    hoodAtPosition = hoodError <= HoodConstants.ACCEPTABLE_MOTOR_ERROR.in(Rotations);
+    hoodAtPosition = hoodMotorAngleAbsError <= HoodConstants.ACCEPTABLE_MOTOR_ERROR.in(Rotations);
 
     Logger.recordOutput("Hood/currentHoodAngleDegrees", getCurrentHoodAngle().in(Degrees));
     Logger.recordOutput("Hood/atPosition", hoodAtPosition);
   }
 
   /**
-   * @param angle Angle for the hood to move to.
+   * @param angle Hood related angle for the hood to move to.
    * @return A start end command that stops the motor on completion.
    */
   public Command goToAngleCommand(Angle angle) {
     //    atPosition = withinAcceptableErrorCalculation();
     return new StartEndCommand(() -> goToAngle(angle), this::stopMotor, this)
         .until(this::isHoodAtPosition)
-        .withTimeout(HOOD_MOVEMENT_TIMEOUT);
+        .withTimeout(HoodConstants.HOOD_MOVEMENT_TIMEOUT);
   }
 
   /**
    * Wrapper for the {@link HoodIO#setSetpoint(Angle)} method, taking into account the.
    *
-   * @param angle Angle of the <b>HOOD</b> to move to.
+   * @param angle The angle in relation to the <b>HOOD</b> to for the hood move to.
    */
   public void goToAngle(Angle angle) {
     hoodAtPosition = false;
