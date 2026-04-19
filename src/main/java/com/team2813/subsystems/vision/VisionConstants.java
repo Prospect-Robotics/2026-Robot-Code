2eb36854fdf0c8ff30a8892674ed049dbb3231d9
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-04-19 15:13:30 -0700

    Readded wait until at velocity for shooter auto commands

--- src/main/java/com/team2813/subsystems/vision/VisionConstants.java
+++ src/main/java/com/team2813/subsystems/vision/VisionConstants.java
@@ -11,12 +11,10 @@ import static edu.wpi.first.units.Units.*;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
-import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.wpilibj.Preferences;
-import org.littletonrobotics.junction.Logger;
 
 public class VisionConstants {
 
