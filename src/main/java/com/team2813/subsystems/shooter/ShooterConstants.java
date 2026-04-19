955a5e2d7dee0b330fffd0f9e926c5f03e475641
Spderman3333 <118777573+spderman3333@users.noreply.github.com>
2026-04-18 18:00:50 -0700

    Fixed herd velocity

--- src/main/java/com/team2813/subsystems/shooter/ShooterConstants.java
+++ src/main/java/com/team2813/subsystems/shooter/ShooterConstants.java
@@ -42,8 +42,8 @@ public class ShooterConstants {
   static {
     // Shooter motors.
     Preferences.initDouble(SHOOTER_TRENCH_SHOOT_PREFERENCE_NT, 100);
-    Preferences.initDouble(SHOOTER_HUB_SHOOT_PREFERENCE_NT, 65);
-    Preferences.initDouble(SHOOTER_HERD_SHOOT_PREFERENCE_NT, 60);
+    Preferences.initDouble(SHOOTER_HUB_SHOOT_PREFERENCE_NT, 60);
+    Preferences.initDouble(SHOOTER_HERD_SHOOT_PREFERENCE_NT, 55);
     Preferences.initDouble(SHOOTER_OUTTAKE_PREFERENCE_NT, -5);
   }
 
@@ -103,7 +103,7 @@ public class ShooterConstants {
   }
 
   public static AngularVelocity getShooterHerdShootVelocity() {
-    return RotationsPerSecond.of(Preferences.getDouble(SHOOTER_HERD_SHOOT_PREFERENCE_NT, 60));
+    return RotationsPerSecond.of(Preferences.getDouble(SHOOTER_HERD_SHOOT_PREFERENCE_NT, 55));
   }
 
   public static Voltage getShooterOuttakeVoltage() {
--- src/main/java/com/team2813/util/HubPositionUtil.java
+++ src/main/java/com/team2813/util/HubPositionUtil.java
@@ -1,6 +1,5 @@
 package com.team2813.util;
 
-import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Meters;
 
 import edu.wpi.first.math.geometry.Pose2d;
@@ -19,12 +18,6 @@ public class HubPositionUtil {
   public static final Translation2d RED_HUB_POSITION = new Translation2d(11.812, 4.000);
 
   /**
-   * Used to flip the bot the hub angle by 180 degrees because we shoot toward the back of the bot
-   * now.
-   */
-  private static Rotation2d HALF_ROTATION = new Rotation2d(Degrees.of(180));
-
-  /**
    * Calculates the angle to the hub (based on the current alliance). Defaults to blue hub if no
    * alliance present.
    *
@@ -37,7 +30,7 @@ public class HubPositionUtil {
     return getCurrentHub(currentAlliance)
         .minus(robotPosition.getTranslation())
         .getAngle()
-        .plus(HALF_ROTATION);
+        .plus(Rotation2d.k180deg);
   }
 
   /**
